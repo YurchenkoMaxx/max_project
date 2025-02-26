bd5ecec227946acc9f3b115a7e6801ed9b544932
Max Yurchenko <yurchenko.maksym@gmail.com>
2025-02-26 21:55:55 +0000

    js add

--- src/js/menu.js
+++ src/js/menu.js
@@ -5,11 +5,9 @@
     modal: document.querySelector('[data-menu]'),
     overlay: document.querySelector('[data-menu-overlay]'), // Добавляем оверлей
   };
-
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
   refs.overlay.addEventListener('click', toggleModal); // Закрытие по клику на фон
-
   function toggleModal() {
     refs.modal.classList.toggle('is-open');
     refs.overlay.classList.toggle('is-open'); // Показываем/скрываем фон
