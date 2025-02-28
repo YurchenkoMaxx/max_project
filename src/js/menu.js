bc8f57472ec3ce0d1a961e3e1e5ecd4520f2b34f
annamutu <annamutu27@gmail.com>
2025-02-28 11:57:13 +0200

    7007

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,17 +1,42 @@
 (() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-menu-open]'),
-    closeModalBtn: document.querySelector('[data-menu-close]'),
-    modal: document.querySelector('[data-menu]'),
-    overlay: document.querySelector('[data-menu-overlay]'), // Добавляем оверлей
+  const mobileMenu = document.querySelector('.js-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
+
+  const toggleMenu = () => {
+    const anchors = mobileMenu.querySelectorAll('a[href*="#"]');
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+
+    if (anchors.length === 0) return;
+
+    if (!isMenuOpen) {
+      anchors.forEach(anchor => {
+        anchor.addEventListener('click', toggleMenu);
+      });
+      return;
+    }
+
+    anchors.forEach(anchor => {
+      anchor.removeEventListener('click', toggleMenu);
+    });
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-  refs.overlay.addEventListener('click', toggleModal); // Закрытие по клику на фон
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-open');
-    refs.overlay.classList.toggle('is-open'); // Показываем/скрываем фон
-  }
+  // Вказати брейкпоінт після якого повинна зачинятися
+  window.matchMedia('(min-width: 375px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
 })();
